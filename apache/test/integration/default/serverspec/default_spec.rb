7a0eb9b9622b21c4418f86aa135d002ab7cfab2d
ChefDK User <chef@ip-10-91-181-7.ec2.internal>
2016-06-23 12:43:55 +0000

    Apache test Added

--- apache/test/integration/default/serverspec/default_spec.rb
+++ apache/test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
 describe 'apache::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+
+  describe package('httpd') do
+    it { should be_installed }
+  end
+  describe file('/var/www/html/index.html') do
+    it { should be_file }
+    it { should contain 'Hello World' }
+  end
+  describe service('httpd') do
+    it {should be_enabled}
+    it {should be_running}
+  end
+  describe command('curl localhost') do
+    its(:stdout) { should match /Hello World/ }
   end
 end
--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -11,5 +11,12 @@ package 'git' do
 end
 
 file '/etc/motd' do
-  content 'Property of .....'
+  content 'Property of .....
+  IPADDRESS: 10.91.181.7 
+  HOSTNAME : ip-10-91-181-7
+  MEMORY : 502272 kB
+  CPU : 1799.999'
+  mode '0644'
+  owner 'root'
+  group 'root'
 end
